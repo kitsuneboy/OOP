a585f89f1cedc34a9b3394c2c89035f882d1a634
Luchuk Artem <luchukartem3@gmail.com>
2021-08-19 22:25:41 +0300

    Two strings

---
+++ Task6.1.py
@@ -0,0 +1,8 @@
+# Даны две строки. Выведите на экран символы, которые есть в обоих строках.
+
+first_string = set("zxcvb21")
+second_string = set("qwertyz1")
+print(first_string & second_string)
+# or
+print(first_string.intersection(second_string))
+
